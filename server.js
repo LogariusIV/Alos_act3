b53c830b04f2069ac5d17ae5e2816a2e960b2ad5
Mohamed <hamidouaze@gmail.com>
2022-04-26 12:17:38 +0100

    implementing totoro for versioning api

--- server.js
+++ server.js
@@ -1,2 +1,36 @@
 const express = require('express')
 const app = express()
+//const jwt = require('jsonwebtoken')
+const route1 = require('./api/ver1/route1')
+const route2 = require('./api/ver2/route2')
+const totoro = require('totoro-node')
+
+app.use(express.json())
+
+
+app.use('/laliga', totoro.rain({
+    v1: {
+        endpoints: [
+            {
+                route: "/match",
+                method: "GET",
+                implementation: route1.matchres
+            }
+            
+        ]
+    },
+    v2: {
+        endpoints: [
+            {
+                route: "/match",
+                method: "GET",
+                implementation: route2.matchreswithstat
+            }
+        ]
+    }
+}))
+
+
+app.listen(3000, () => {
+    console.log('Server Started')
+  })
